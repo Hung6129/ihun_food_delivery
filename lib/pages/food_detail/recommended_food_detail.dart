d4e97197241583d016de4356a576a98aa8ff048c
HungNguyenPQ <quyhung6129@gmail.com>
2023-07-12 00:40:52 +0700

    minor update for detail pages

--- lib/pages/food_detail/recommended_food_detail.dart
+++ lib/pages/food_detail/recommended_food_detail.dart
@@ -1,17 +1,10 @@
-import 'package:badges/badges.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:ihun_food_delivery/config/palettes.dart';
-import 'package:ihun_food_delivery/config/text_styles.dart';
+
 import 'package:ihun_food_delivery/controllers/cart_controller.dart';
 import 'package:ihun_food_delivery/controllers/popular_product_controller.dart';
 import 'package:ihun_food_delivery/controllers/recommended_product_controller.dart';
-import 'package:ihun_food_delivery/core/app_constants.dart';
 
-import 'package:ihun_food_delivery/config/dimension.dart';
-import 'package:badges/badges.dart' as badges;
-import 'package:ihun_food_delivery/config/routes/route_helper.dart';
 import 'package:ihun_food_delivery/pages/food_detail/widgets/food_detail_widgets.dart';
 
 class RecommendedFoodDetail extends StatelessWidget {
@@ -29,89 +22,11 @@ class RecommendedFoodDetail extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: <Widget>[
-          SliverAppBar(
-            pinned: true,
-            snap: false,
-            floating: false,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.vertical(
-                bottom: Radius.circular(Dimensions.height30),
-              ),
-            ),
-            expandedHeight: Dimensions.backGroundDetailImage,
-            stretch: true,
-            actions: [
-              GetBuilder<PopularProductController>(
-                builder: ((controller) {
-                  return (Get.find<PopularProductController>().totalItems >= 1
-                      ? badges.Badge(
-                          badgeStyle: const BadgeStyle(badgeColor: Palettes.p1),
-                          badgeAnimation: const badges.BadgeAnimation.slide(),
-                          badgeContent: controller.totalItems >= 1
-                              ? Text(
-                                  controller.totalItems.toString(),
-                                  style: TextStyles.defaultStyle.whiteText,
-                                )
-                              : const SizedBox(),
-                          child: IconButton(
-                            onPressed: () {
-                              Get.toNamed(RoutesHelper.getCart());
-                            },
-                            icon: const Icon(Icons.shopping_cart_outlined, color: Colors.white),
-                          ),
-                        )
-                      : IconButton(
-                          onPressed: () {
-                            Get.toNamed(RoutesHelper.getCart());
-                          },
-                          icon: const Icon(Icons.shopping_cart_outlined, color: Colors.white),
-                        ));
-                }),
-              ),
-              SizedBox(width: Dimensions.height20)
-            ],
-            flexibleSpace: FlexibleSpaceBar(
-              stretchModes: const [StretchMode.zoomBackground],
-              background: CachedNetworkImage(
-                imageUrl: AppConstants.BASE_URL + AppConstants.UPLOAD_URI + product.img!,
-                imageBuilder: (context, imageProvider) => Container(
-                  decoration: BoxDecoration(
-                    borderRadius:
-                        const BorderRadius.only(bottomLeft: Radius.circular(30), bottomRight: Radius.circular(30)),
-                    image: DecorationImage(
-                      image: imageProvider,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                placeholder: (context, url) => const Center(
-                  child: CircularProgressIndicator(),
-                ),
-                errorWidget: (context, url, error) => const Icon(Icons.error),
-              ),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(Dimensions.height10),
-                  child: Text(
-                    product.name!,
-                    style: TextStyle(fontSize: Dimensions.font26),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(Dimensions.height10),
-                  child: Text(product.description!),
-                ),
-              ],
-            ),
-          ),
+          sliverAppBarFoodDetail(product),
+          sliverToBoxAdapterFoodDetail(product),
         ],
       ),
-      bottomSheet: bottomSheetFoodToCart(context, product),
+      bottomSheet: bottomSheetFoodDetailToCart(context, product),
     );
   }
 }
--- lib/pages/food_detail/widgets/food_detail_widgets.dart
+++ lib/pages/food_detail/widgets/food_detail_widgets.dart
@@ -1,11 +1,101 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:ihun_food_delivery/config/palettes.dart';
+import 'package:ihun_food_delivery/config/routes/route_helper.dart';
+import 'package:ihun_food_delivery/config/text_styles.dart';
 import 'package:ihun_food_delivery/controllers/popular_product_controller.dart';
+import 'package:ihun_food_delivery/core/app_constants.dart';
 import 'package:ihun_food_delivery/model/product.dart';
+import 'package:badges/badges.dart' as badges;
 
-Widget bottomSheetFoodToCart(BuildContext context, ProductModel product) {
+Future showDialog(BuildContext context, ProductModel product) {
+  return showModalBottomSheet<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return GetBuilder<PopularProductController>(
+          builder: (controller) => SizedBox(
+                height: 280.h,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
+                      child: Text(
+                        'Adding ${product.name!} to your cart',
+                        style: TextStyle(fontSize: 17.sp),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            child: Text(
+                              '-',
+                              style: TextStyle(fontSize: 26.sp),
+                            ),
+                            onPressed: () => controller.setQuantity(false),
+                          ),
+                          SizedBox(
+                            width: 15.w,
+                          ),
+                          Text(controller.inCartItems.toString(), style: TextStyle(fontSize: 26.sp)),
+                          SizedBox(width: 15.w),
+                          ElevatedButton(
+                            child: Text('+', style: TextStyle(fontSize: 26.sp)),
+                            onPressed: () => controller.setQuantity(true),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Column(
+                        children: [
+                          const Divider(),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              ElevatedButton(
+                                style: ButtonStyle(
+                                    padding: MaterialStateProperty.all(EdgeInsets.all(10.h)),
+                                    backgroundColor: MaterialStateProperty.all(Palettes.p2)),
+                                child: Text('Add To Cart', style: TextStyle(color: Colors.white, fontSize: 18.sp)),
+                                onPressed: () {
+                                  controller.addItems(product);
+                                  Navigator.pop(context);
+                                },
+                              ),
+                              ElevatedButton(
+                                style: ButtonStyle(
+                                  padding: MaterialStateProperty.all(EdgeInsets.all(10.h)),
+                                  backgroundColor: MaterialStateProperty.all(Colors.redAccent),
+                                ),
+                                child: Text(
+                                  'Cancel',
+                                  style: TextStyle(color: Colors.white, fontSize: 18.sp),
+                                ),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ));
+    },
+  );
+}
+
+Widget bottomSheetFoodDetailToCart(BuildContext context, ProductModel product) {
   return Container(
     decoration: ShapeDecoration(
       shape: RoundedRectangleBorder(
@@ -14,10 +104,10 @@ Widget bottomSheetFoodToCart(BuildContext context, ProductModel product) {
           topRight: Radius.circular(30.h),
         ),
       ),
-      color: Palettes.p1,
+      color: Palettes.p4,
     ),
     width: double.maxFinite,
-    height: 120.h,
+    height: 100.h,
     child: Padding(
       padding: EdgeInsets.all(10.h),
       child: Row(
@@ -28,90 +118,10 @@ Widget bottomSheetFoodToCart(BuildContext context, ProductModel product) {
             flex: 2,
             child: ElevatedButton(
               style: ButtonStyle(
-                  padding: MaterialStateProperty.all(EdgeInsets.all(20.h)),
-                  textStyle: MaterialStateProperty.all(TextStyle(fontSize: 20.sp))),
+                  padding: MaterialStateProperty.all(EdgeInsets.all(15.h)),
+                  textStyle: MaterialStateProperty.all(TextStyle(fontSize: 18.sp))),
               onPressed: () {
-                showModalBottomSheet<void>(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return GetBuilder<PopularProductController>(
-                        builder: (controller) => SizedBox(
-                              height: 350.h,
-                              child: Column(
-                                children: [
-                                  SizedBox(height: 45.h),
-                                  Text(
-                                    'Adding ${product.name!} to your cart',
-                                    style: TextStyle(fontSize: 20.sp),
-                                  ),
-                                  Expanded(
-                                    flex: 1,
-                                    child: Row(
-                                      crossAxisAlignment: CrossAxisAlignment.center,
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      children: [
-                                        ElevatedButton(
-                                          child: Text(
-                                            '-',
-                                            style: TextStyle(fontSize: 26.sp),
-                                          ),
-                                          onPressed: () => controller.setQuantity(false),
-                                        ),
-                                        SizedBox(
-                                          width: 15.w,
-                                        ),
-                                        Text(controller.inCartItems.toString(), style: TextStyle(fontSize: 26.sp)),
-                                        SizedBox(width: 15.w),
-                                        ElevatedButton(
-                                          child: Text('+', style: TextStyle(fontSize: 26.sp)),
-                                          onPressed: () => controller.setQuantity(true),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  Expanded(
-                                    flex: 1,
-                                    child: Column(
-                                      children: [
-                                        const Divider(),
-                                        Row(
-                                          crossAxisAlignment: CrossAxisAlignment.center,
-                                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                          children: [
-                                            ElevatedButton(
-                                              style: ButtonStyle(
-                                                  padding: MaterialStateProperty.all(EdgeInsets.all(20.h)),
-                                                  backgroundColor: MaterialStateProperty.all(Palettes.p1)),
-                                              child: Text('Add To Cart',
-                                                  style: TextStyle(color: Colors.white, fontSize: 20.sp)),
-                                              onPressed: () {
-                                                controller.addItems(product);
-                                                Navigator.pop(context);
-                                              },
-                                            ),
-                                            ElevatedButton(
-                                              style: ButtonStyle(
-                                                padding: MaterialStateProperty.all(EdgeInsets.all(20.h)),
-                                                backgroundColor: MaterialStateProperty.all(Colors.redAccent),
-                                              ),
-                                              child: Text(
-                                                'Cancel',
-                                                style: TextStyle(color: Colors.white, fontSize: 20.sp),
-                                              ),
-                                              onPressed: () {
-                                                Navigator.pop(context);
-                                              },
-                                            ),
-                                          ],
-                                        ),
-                                      ],
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ));
-                  },
-                );
+                showDialog(context, product);
               },
               child: const Text(
                 'Add To Cart',
@@ -123,8 +133,8 @@ Widget bottomSheetFoodToCart(BuildContext context, ProductModel product) {
             flex: 1,
             child: ElevatedButton.icon(
               style: ButtonStyle(
-                  padding: MaterialStateProperty.all(EdgeInsets.all(20.h)),
-                  textStyle: MaterialStateProperty.all(TextStyle(fontSize: 20.sp))),
+                  padding: MaterialStateProperty.all(EdgeInsets.all(15.h)),
+                  textStyle: MaterialStateProperty.all(TextStyle(fontSize: 18.sp))),
               label: const Text('+'),
               onPressed: () {},
               icon: const Icon(Icons.favorite),
@@ -132,6 +142,89 @@ Widget bottomSheetFoodToCart(BuildContext context, ProductModel product) {
           ),
         ],
       ),
+    ),
+  );
+}
+
+Widget sliverAppBarFoodDetail(ProductModel product) {
+  return SliverAppBar(
+    pinned: true,
+    snap: false,
+    floating: false,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.vertical(
+        bottom: Radius.circular(30.h),
+      ),
+    ),
+    expandedHeight: 280.h,
+    stretch: true,
+    actions: [
+      GetBuilder<PopularProductController>(
+        builder: ((controller) {
+          return Get.find<PopularProductController>().totalItems >= 1
+              ? badges.Badge(
+                  onTap: () => Get.toNamed(RoutesHelper.getCart()),
+                  badgeStyle: const badges.BadgeStyle(badgeColor: Palettes.p2),
+                  badgeAnimation: const badges.BadgeAnimation.slide(),
+                  badgeContent: controller.totalItems >= 1
+                      ? Text(
+                          controller.totalItems.toString(),
+                          style: TextStyles.defaultStyle.whiteText,
+                        )
+                      : const SizedBox(),
+                  child: Icon(Icons.shopping_bag_rounded, color: Palettes.p2, size: 30.h),
+                )
+              : GestureDetector(
+                  onTap: () => Get.toNamed(RoutesHelper.getCart()),
+                  child: Icon(
+                    Icons.shopping_bag_rounded,
+                    color: Palettes.p1,
+                    size: 30.h,
+                  ),
+                );
+        }),
+      ),
+      SizedBox(width: 20.w)
+    ],
+    flexibleSpace: FlexibleSpaceBar(
+      stretchModes: const [StretchMode.zoomBackground],
+      background: CachedNetworkImage(
+        imageUrl: AppConstants.BASE_URL + AppConstants.UPLOAD_URI + product.img!,
+        imageBuilder: (context, imageProvider) => Container(
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(30), bottomRight: Radius.circular(30)),
+            image: DecorationImage(
+              image: imageProvider,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        placeholder: (context, url) => const Center(
+          child: CircularProgressIndicator(),
+        ),
+        errorWidget: (context, url, error) => const Icon(Icons.error),
+      ),
+    ),
+  );
+}
+
+Widget sliverToBoxAdapterFoodDetail(ProductModel product) {
+  return SliverToBoxAdapter(
+    child: Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(10.h),
+          child: Text(
+            product.name!,
+            style: TextStyle(fontSize: 26.sp),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(10.h),
+          child: Text(product.description!),
+        ),
+      ],
     ),
   );
 }
